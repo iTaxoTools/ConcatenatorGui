c76f80d0553c7161ad5f76fb63ec7cd72ba3b867
StefanPatman <stefanpatman91@gmail.com>
2021-10-15 17:36:13 +0300

    Fix pyinstaller files

--- scripts/concatenator.py
+++ scripts/concatenator.py
@@ -4,9 +4,9 @@
 """Launch the Concatenator GUI"""
 
 import multiprocessing
-from itaxotools.concatenator_gui import main
+from itaxotools.concatenator_gui import run
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     multiprocessing.set_start_method('spawn')
-    main()
+    run()
--- src/itaxotools/__pyinstaller/hook-itaxotools.concatenator_gui.py
+++ src/itaxotools/__pyinstaller/hook-itaxotools.concatenator_gui.py
@@ -1,5 +1,4 @@
 # https://pyinstaller.readthedocs.io/en/stable/hooks.html
 
 from PyInstaller.utils.hooks import collect_data_files
-datas = collect_data_files('itaxotools.concatenator')
-datas += collect_data_files('itaxotools.concatenator.gui')
+datas = collect_data_files('itaxotools.concatenator.gui')
