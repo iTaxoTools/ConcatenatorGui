fd805ca2df8d1e5e1fb4f1ab299d931ce2685736
StefanPatman <stefanpatman91@gmail.com>
2023-12-18 14:02:04 +0200

    Update trim step, add dummy trim operator

--- src/itaxotools/concatenator_gui/steps/trim.py
+++ src/itaxotools/concatenator_gui/steps/trim.py
@@ -22,6 +22,10 @@ from PySide6 import QtCore
 from PySide6 import QtWidgets
 from PySide6 import QtGui
 
+from tempfile import TemporaryDirectory
+from itertools import chain
+from typing import Optional
+from pathlib import Path
 from time import sleep
 
 from itaxotools import common
@@ -30,10 +34,20 @@ import itaxotools.common.resources # noqa
 
 from itaxotools.common.utility import AttrDict
 
-from itaxotools.concatenator.library.model import GeneSeries
+from itaxotools.concatenator import (
+    FileType, FileFormat, GeneStream, FileWriter,
+    get_writer, get_extension, read_from_path)
+from itaxotools.concatenator.library.model import GeneSeries, Operator
 from itaxotools.concatenator.library.operators import OpUpdateMetadata
 from itaxotools.concatenator.library.codons import (
     GeneticCode, ReadingFrame, _GC_DESCRIPTIONS)
+from itaxotools.concatenator.library.operators import (
+    OpChainGenes, OpTranslateGenes, OpApplyToGene, OpTagSet,
+    OpUpdateMetadata, OpFilterGenes, OpGeneralInfo,
+    OpGeneralInfoPerFile, OpGeneralInfoPerGene,
+    OpGeneralInfoTagMafftRealigned,
+    OpGeneralInfoTagPaddedLength,
+    OpGeneralInfoTagPaddedCodonPosition)
 
 from .. import model
 from .. import widgets
@@ -41,6 +55,18 @@ from .. import step_state_machine as ssm
 
 from .wait import StepWaitBar
 from .align import RichRadioButton
+
+
+class OpTrim(Operator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.genes = set()
+
+    def call(self, gene: GeneSeries) -> Optional[GeneSeries]:
+        print(f'Trimming: {gene.name}')
+        self.genes.add(gene.name)
+        sleep(1)
+        return gene
 
 
 class StepTrimEdit(ssm.StepTriStateEdit):
@@ -62,7 +88,8 @@ class StepTrimEdit(ssm.StepTriStateEdit):
         clipkit = RichRadioButton('Clipkit:', 'only keep phylogenetically informative sites.', widget)
         skip = QtWidgets.QRadioButton('Skip trimming', widget)
         skip.setStyleSheet("QRadioButton { letter-spacing: 1px; }")
-        skip.setChecked(True)
+        clipkit.setEnabled(False)
+        gblocks.setChecked(True)
 
         radios = QtWidgets.QVBoxLayout()
         radios.addWidget(gblocks)
@@ -124,19 +151,75 @@ class StepTrim(ssm.StepTriState):
     StepDone = StepTrimDone
     StepFail = StepTrimFail
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.temp_cache = None
+        self.charsets_cached = set()
+        self.method_last = None
+        self.process = None
+
     def onEntry(self, event):
         super().onEntry(event)
+        input_update = self.machine().states.input.timestamp_get()
         last_filter_update = self.machine().states.filter.timestamp_get()
-        if last_filter_update > self.timestamp_get():
+        align_options_update = self.machine().states.align_options.timestamp_get()
+        align_sets_update = self.machine().states.align_sets.timestamp_get()
+        last_update = max(input_update, last_filter_update, align_options_update, align_sets_update)
+        if last_update > self.timestamp_get():
+            self.temp_cache = TemporaryDirectory(prefix='concat_trim_cache_')
+            self.charsets_cached = set()
+
+    def onExit(self, event):
+        super().onExit(event)
+        method = self.states.edit.get_method()
+        if method != self.method_last:
             self.timestamp_set()
 
     def work(self):
-        with self.states['wait'].redirect():
-            self.update(0, 0, 'Getting ready...')
-            print('Pretend to do some actual work...')
-            sleep(2)
-            print('Done pretending!')
-            self.update(1, 1, 'text')
+        charsets = {
+            k for k, v in self.machine().states.input.data.charsets.items()
+            if v.translation is not None and v.samples
+            and v.name not in self.charsets_cached}
+        method = self.states.edit.get_method()
+        with self.states.wait.redirect():
+            if not charsets and method == self.method_last:
+                print('NOTHING TO DO')
+                return
+            stream = self.work_get_stream()
+            self.work_trim(stream)
+            self.method_last = method
+
+    def work_trim(self, stream: GeneStream):
+        self.update(0, 0, 'Getting ready...')
+        print('Pretend to do some actual work...')
+        operator = OpTrim()
+        stream = stream.pipe(operator)
+        for gene in stream:
+            pass
+        self.charsets_cached = {
+            k for k, v in self.machine().states.input.data.charsets.items()
+            if v.translation is not None and k in operator.genes}
+        print('Done pretending!')
+        self.update(1, 1, 'text')
+
+    def work_get_stream(self):
+        input_streams = [
+            read_from_path(file.path)
+            for file in self.machine().states.input.data.files.values()]
+        aligned_cache = Path(self.machine().states.align_sets.temp_cache.name)
+        aligned_charsets = {
+            k for k, v in self.machine().states.input.data.charsets.items()
+            if v.aligned and v.translation is not None}
+        aligned_stream = (
+            read_from_path(aligned_cache)
+            .pipe(OpFilterGenes(aligned_charsets))
+            )
+        all_streams = [aligned_stream] + input_streams
+        stream = (
+            GeneStream(chain(*all_streams))
+            .pipe(OpChainGenes())
+            )
+        return stream
 
     def skipWait(self):
         method = self.states.edit.get_method()
