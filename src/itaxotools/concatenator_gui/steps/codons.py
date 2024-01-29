b50f524b53e46b0dc4283090443ddeab37e3a109
StefanPatman <stefanpatman91@gmail.com>
2024-01-29 15:10:53 +0200

    Update copyright notice to 2021-2024

--- src/itaxotools/concatenator_gui/steps/codons.py
+++ src/itaxotools/concatenator_gui/steps/codons.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 # ConcatenatorQt - GUI for Concatenator
-# Copyright (C) 2021-2023  Patmanidis Stefanos
+# Copyright (C) 2021-2024  Patmanidis Stefanos
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
